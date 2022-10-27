333987b017b72268b0f2943cc061ee74588f86f8
alex231 <alex231@ukr.net>
2022-10-27 12:16:45 +0300

    add word count feature

--- visereverce/visereverce/views.py
+++ visereverce/visereverce/views.py
@@ -9,7 +9,12 @@ def home(request):
 def reverse(request):
     user_text = request.GET['user_text']
     print(user_text)
+    new_list = user_text.split()
+    print(new_list)
+    len_list = len(new_list)
+    print(len_list)
     reverse_text = user_text[::-1]
     print(reverse_text)
-    return render(request, 'reverse.html', {'user_text': user_text, 'reverse_text': reverse_text})
+    return render(request, 'reverse.html', {'user_text': user_text, 'reverse_text': reverse_text, 'len_list': len_list})
+
 
