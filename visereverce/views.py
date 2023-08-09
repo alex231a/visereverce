c247fe6a039d12eca202893ee8e73fd638f33f10
oyakovenko <alex231@ukr.net>
2023-08-09 21:38:46 +0300

    add calculation feature

--- visereverce/views.py
+++ visereverce/views.py
@@ -12,3 +12,29 @@ def reverse(request):
     len_list = len(list_text_origin)
     reversed_text = origin_text[::-1]
     return render(request, 'reverse.html', {'origin_test': origin_text, 'reversed_text': reversed_text, 'len_list': len_list})
+
+
+def calc(request):
+    return render(request, 'calc.html')
+
+
+def result(request):
+    val1 = request.GET['val1']
+    val2 = request.GET['val2']
+    val1 = int(val1)
+    val2 = int(val2)
+    action = request.GET['action']
+    if action == '+':
+        res = val1+val2
+    elif action == '-':
+        res = val1 - val2
+    elif action == '*':
+        res = val1 * val2
+    elif action == '/' and val2 != 0:
+        res = val1/val2
+    else:
+        res = 'Wrong value'
+    return render(request, 'result.html',
+                  {'val1': val1, 'val2': val2, 'action': action, 'res': res})
+
+
